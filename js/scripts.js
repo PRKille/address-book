3e03d7ffbb4cbc6624de5fb2ca0eeccc99c13758
Geoff Goetz and Patrick Kille <geoff@>
2020-01-15 16:39:45 -0800

    create user filled uls

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
-  $("#addressInput").submit(function(event) {
+  $("form#addressInput").submit(function(event) {
     var bookName = $("input#name").val();
     var bookAdd1 = $("input#address1").val();
-    var bookAdd2 = $("input#address2").val():
+    var bookAdd2 = $("input#address2").val();
     var phone = $("input#phone").val();
     var email = $("input#email").val();
 
-    $("div.well").after(bookName);
+    $("#contacts").prepend("<ul>" + bookName + "</ul>").append("<li>" + bookAdd1 + "</li>" + "<li>" + bookAdd2 + "</li>" + "<li>" + phone + "</li>" + "<li>" + email + "</li>");
 
-    $("#bookinfo").show();
+    $("li").hide();
+    $("#contacts").click(function() {
+      $("li", this).toggle();
+    });
 
     event.preventDefault();
   });
